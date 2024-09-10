5fc6ae41a9a6ce68a5a6d3090a793d509606e7ef
MohamedOsamaGalal98 <146726249+MohamedOsamaGalal98@users.noreply.github.com>
2024-09-10 20:54:43 +0300

    finished modifing master module

---
+++ app/Models/Note.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'note',
+        'user_id',
+    ];
+
+    
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+}
